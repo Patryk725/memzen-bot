03f62ffbe8344014cccd7ca6abcd17775caa856f
Patryk <68645604+Patryk725@users.noreply.github.com>
2025-01-06 12:39:31 +0100

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+const { REST, Routes } = require('discord.js');
+const token = process.env['token'];
+const clientId = process.env['app_id'];
+const guildId = process.env['server_id'];
+const fs = require('fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands/utility').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/utility/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Rejestracja komend aplikacji...');
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+    console.log('Komendy zostały zarejestrowane!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
